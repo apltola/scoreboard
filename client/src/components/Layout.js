307ce6693b1fc35155734d27bd61072a2ce2d322
allu <aleksijaakkojuhani@gmail.com>
2020-10-23 12:08:36 +0300

    adding frontend layout

---
+++ client/src/components/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/Layout.css';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="container">
+      <header className="app-header">scoreboard.app</header>
+      <main className="main">
+        <div className="page-content">{children}</div>
+      </main>
+      <footer className="app-footer">
+        check the code on{' '}
+        <a
+          href="https://github.com/apltola/scoreboard"
+          target="_blank"
+          rel="noopener"
+        >
+          github
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
